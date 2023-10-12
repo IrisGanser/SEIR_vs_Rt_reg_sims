28b1569b22a7e8602ba63040335ccc2cd8185d9d
Iris Ganser <iris.ganser@mail.mcgill.ca>
2023-10-12 16:30:21 -0400

    first commit

---
+++ SEIRAHD_Simulx_boot_2params_new2_1.R
@@ -0,0 +1,89 @@
+library(tidyverse)
+library(colorspace)
+library(magrittr)
+library(kableExtra)
+
+
+setwd("~/PhD/COVID_France/SEIR_vs_Rt_sims/boot_sim_2params_all_new2_Simulx_SEIRAHD")
+source("~/PhD/COVID_France/Dropbox_iris_covid/departement/Données_SPF/Data/data_functions.R")
+source("~/PhD/COVID_France/SEIR_vs_Rt_sims/SEIR_vs_Rt_reg_sims/useful_functions.R")
+
+
+dir3 <- "~/PhD/COVID_France/SEIR_vs_Rt_sims/boot_point_estimates"
+
+# bootstrap replicates
+# j = data simulation
+# i = bootstrap replicate
+
+SEIRAHD_boot_list_all <- list()
+point_est_list <- list()
+popparams <- c()
+
+# load bootstrap population parameters and calculate bootstrap CIs in 2 ways
+for(j in 1:100){
+  params_sim_rep <- list()
+  
+  # load population parameters from individual bootstrap runs 
+  for(i in 1:100){
+    rm(popparams)
+    try(popparams <- read.table(paste0(getwd(), "/SEIRAHD_Simulx_new2_2params_", j, "boot", i, "/populationParameters.txt"), 
+                                header = TRUE, sep = ",") %>%
+          bootstrap_cleaning(., boot_rep = i, sim_rep = j))
+    
+    
+    if(exists("popparams")){
+      params_sim_rep[[i]] <- popparams
+    }
+  }
+  
+  # calculate CIs
+  pop_params_boot <- bootstrap_CI_calc(params_sim_rep)
+  
+  
+  # load point estimates
+  point_est <- read.table(paste0(dir3, "/SEIRAHD_Simulx_new2_pe_", j, "/populationParameters.txt"), 
+                          header = TRUE, sep = ",") %>%
+    popparam_cleaning() %>%
+    mutate(sim_rep = j) %>%
+    rename(mean_est2 = value)
+  
+  
+  SEIRAHD_boot_list_all[[j]] <- pop_params_boot %>%
+    ungroup() %>%
+    left_join(point_est, by = c("parameter", "sim_rep")) %>%
+    select(parameter, sim_rep, mean_est1, mean_est2, CI_LL1, CI_LL2, CI_UL1, CI_UL2, sd_est)
+  
+}
+
+save(SEIRAHD_boot_list_all, file = "SEIRAHD_boot_list_2params_new2_Simulx.RData")
+
+
+
+# join point estimates and pivot into longer format 
+SEIRAHD_boot_df_long <- do.call("rbind.data.frame", SEIRAHD_boot_list_all) %>%
+  pivot_longer(cols = -c(parameter, sim_rep, sd_est), 
+               names_to = c(".value", "method"), 
+               names_pattern = "(.*)(\\d+)") %>%
+  mutate(parameter = factor(parameter, 
+                            levels = c("beta_ld1", "beta_BG1"),
+                            labels = c("Lockdown", "Barrier gestures")), 
+         true_value = ifelse(parameter == "Lockdown", -1.45, -0.5), 
+         method = ifelse(method == 1, "Bootstrap SE", "Empirical bootstrap"))
+
+
+# plot to compare the two CI methods
+ggplot(SEIRAHD_boot_df_long, aes(ymin = CI_LL, ymax = CI_UL, x = sim_rep, y = mean_est, col = method)) + 
+  geom_pointrange(position = position_dodge(width = 0.3)) + 
+  scale_x_continuous(expand = c(0.01, 0.01)) + 
+  facet_wrap(~parameter, ncol = 1, scale = "free_y") +
+  geom_line(aes(y = true_value), linetype = "dashed", col = "darkred", linewidth = 0.8) + 
+  labs(title= "Bootstrap SEIRAHD Simulx new2", 
+       x = "simulation dataset", y = "coefficient value") +
+  theme_bw() +
+  scale_color_brewer(palette = "Dark2")
+
+
+
+
+
+
