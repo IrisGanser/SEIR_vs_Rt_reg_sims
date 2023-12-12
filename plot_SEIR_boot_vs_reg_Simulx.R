3f13eafbf57978a30cbca11081c0fb70377c8ed2
Iris Ganser <iris.ganser@mail.mcgill.ca>
2023-12-12 21:22:58 +0100

    added summary plot

--- plot_SEIR_boot_vs_reg_Simulx.R
+++ plot_SEIR_boot_vs_reg_Simulx.R
@@ -30,8 +30,8 @@ SEIR_boot_df_comp <- bootstrap_summary(SEIR_boot_list_all)
 SEIRAHD_boot_df_comp <- bootstrap_summary(SEIRAHD_boot_list_all) 
 
 reg_res_2params_Simulx_new2 <- reg_res_I_2params_all_Simulx_df %>%
-  mutate(model = "Regression model IncI") %>%
-  bind_rows(reg_res_H_2params_all_Simulx_df %>% mutate(model = "Regression model IncH")) %>%
+  mutate(model = "Regression model cases") %>%
+  bind_rows(reg_res_H_2params_all_Simulx_df %>% mutate(model = "Regression model hospitalizations")) %>%
   reg_summary()
 
 
@@ -49,8 +49,9 @@ comp_cols <- c(br_palette[c(1, 5)], "black", br_palette[c(16, 20)])
 ggplot(comp_df_Simulx, aes(x = sim_rep, y = mean_est, ymin = CI_LL, ymax = CI_UL, col = model)) + 
   geom_pointrange(position = position_dodge(width = 0.8)) +
   geom_line(aes(y = true_value), col = "black", linetype = "dashed", linewidth = 0.8) + 
+  scale_x_continuous(expand = c(0.01, 0.01)) +
   facet_wrap(~parameter, scales = "free_y", nrow = 2) +
-  labs(title = "95% CIs, SEIR-type models vs. 2-step regression models", 
+  labs(#title = "95% CIs, SEIR-type models vs. 2-step regression models", 
        x = "Simulation data set", y = "Coefficient value", col = "") + 
   scale_color_manual(values = comp_cols[-3]) +
   theme_bw() +
@@ -58,9 +59,11 @@ ggplot(comp_df_Simulx, aes(x = sim_rep, y = mean_est, ymin = CI_LL, ymax = CI_UL
         axis.title = element_text(family = "serif", size = 15), 
         axis.text.x = element_text(family = "serif", size = 13), 
         axis.text.y = element_text(family = "serif", size = 13), 
-        legend.text = element_text(family = "serif", size = 14.5))
+        legend.text = element_text(family = "serif", size = 14.5), 
+        legend.position = "bottom", 
+        strip.text = element_text(family = "serif", size = 14.5))
 
-ggsave("SEIR_vs_reg_Simulx.jpeg", dpi = 400, width = 14, height = 8.5)
+ggsave("SEIR_vs_reg_Simulx_wo_title.jpeg", dpi = 400, width = 14, height = 8.5)
 
 
 # metrics table
@@ -83,11 +86,36 @@ metric_df_Simulx <- comp_df_Simulx %>%
 metric_df_Simulx %>% 
   ungroup() %>%
   select(-parameter) %>%
-  kable(digits = 2, format = "html") %>%
+  kable(digits = 2, format = "latex") %>%
   kable_styling(bootstrap_options = "striped", full_width = FALSE) %>%
   pack_rows("NPI 1", 1, 3) %>%
   pack_rows("NPI 2", 4, 6)
 
+
+
+# only summary plot
+comp_df_Simulx_short <- comp_df_Simulx %>%
+  group_by(parameter, model) %>%
+  summarize(mean = mean(mean_est), CI_LL = mean(CI_LL), CI_UL = mean(CI_UL))
+
+
+ggplot(comp_df_Simulx_short, aes(x = model, y = mean, ymin = CI_LL, ymax = CI_UL, col = model)) + 
+  geom_pointrange() +
+  geom_hline(data = data.frame(true_value = c(-1.45, -0.5), parameter = c("NPI 1", "NPI 2")), 
+             aes(yintercept = true_value), linetype = "dashed") + 
+  facet_wrap(~parameter, scales = "free_y", nrow = 1) +
+  labs(#title = "95% CIs, SEIR-type models vs. 2-step regression models", 
+    y = "Coefficient value", col = "") + 
+  scale_color_manual(values = comp_cols[-3]) +
+  theme_bw() +
+  theme(plot.title = element_text(family = "serif", size = 20), 
+        axis.title.y = element_text(family = "serif", size = 15), 
+        axis.text.x = element_blank(), 
+        axis.title.x = element_blank(), 
+        axis.ticks.x = element_blank(),
+        axis.text.y = element_text(family = "serif", size = 13), 
+        legend.text = element_text(family = "serif", size = 14.5), 
+        legend.position = "bottom")
 
 
 #### Simulx 3 plots ####
@@ -170,6 +198,9 @@ load(paste0(dir, "/reg_res_H_2params_all_Simulx_df4.RData"))
 load(paste0(dir, "/reg_res_I_2params_all_Simulx_df4_m10.RData"))
 load(paste0(dir, "/reg_res_H_2params_all_Simulx_df4_m10.RData"))
 
+SEIR_boot_df_comp <- bootstrap_summary(SEIR_boot_list_all) 
+SEIRAHD_boot_df_comp <- bootstrap_summary(SEIRAHD_boot_list_all) 
+
 
 reg_res_2params_Simulx_new4 <- reg_res_I_2params_all_Simulx_df4 %>%
   mutate(model = "Regression model IncI") %>%
@@ -249,3 +280,69 @@ metric_df_Simulx4 %>%
   kable_styling(bootstrap_options = "striped") %>%
   pack_rows("NPI 1", 1, 3) %>%
   pack_rows("NPI 2", 4, 6)
+
+
+
+# combine with SEIR model from Simulx 2
+SEIR_boot_df_comp <- bootstrap_summary(SEIR_boot_list_all) 
+SEIRAHD_boot_df_comp <- bootstrap_summary(SEIRAHD_boot_list_all) 
+
+reg_res_2params_Simulx_new_4and2 <- reg_res_I_2params_all_Simulx_df %>%
+  mutate(model = "Regression model IncI") %>%
+  bind_rows(reg_res_H_2params_all_Simulx_df %>% mutate(model = "Regression model IncH")) %>%
+  bind_rows(reg_res_I_2params_all_Simulx_df4 %>% mutate(model = "Regression model IncI long")) %>%
+  bind_rows(reg_res_H_2params_all_Simulx_df4 %>% mutate(model = "Regression model IncH long")) %>%
+  reg_summary(true_val_NPI2 = -0.5) %>% 
+  rename(sim_rep = rep, mean_est = value)
+
+
+comp_df_Simulx_4and2 <- SEIR_boot_df_comp %>%
+  mutate(model = "SEIR model") %>%
+  bind_rows(SEIRAHD_boot_df_comp %>% mutate(model = "SEIRAHD model")) %>%
+  rename(mean_est = mean_est2, CI_LL = CI_LL2, CI_UL = CI_UL2) %>%
+  bind_rows(reg_res_2params_Simulx_new_4and2) 
+
+
+plot_cols_paired <- c(brewer.pal(3, "Set2"), brewer.pal(3, "Dark2"))[c(1, 4, 2, 5, 3, 6)]
+
+ggplot(comp_df_Simulx_4and2, aes(x = sim_rep, y = mean_est, ymin = CI_LL, ymax = CI_UL, col = model)) + 
+  geom_pointrange(position = position_dodge(width = 0.8)) +
+  geom_line(aes(y = true_value), col = "black", linetype = "dashed", linewidth = 0.8) + 
+  facet_wrap(~parameter, scales = "free_y", nrow = 2) +
+  labs(title = "SEIR model vs. 2-step regression models", 
+       x = "Simulation data set", y = "Coefficient value", col = "") + 
+  scale_color_manual(values = plot_cols_paired) +
+  theme_bw() +
+  theme(plot.title = element_text(family = "serif", size = 20), 
+        axis.title = element_text(family = "serif", size = 15), 
+        axis.text.x = element_text(family = "serif", size = 13), 
+        axis.text.y = element_text(family = "serif", size = 13), 
+        legend.text = element_text(family = "serif", size = 14.5))
+
+
+# metrics table
+# bring into right format for metrics table
+metric_df_Simulx_4and2 <- comp_df_Simulx_4and2 %>%
+  dplyr::select(parameter, model, perc_CI_covers, mean_bias, mean_rel_bias) %>%
+  unique() %>%
+  arrange(model) %>%
+  rename(coverage = perc_CI_covers, bias = mean_bias, relbias = mean_rel_bias) %>%
+  pivot_longer(cols = c(coverage, bias, relbias), 
+               names_to = "metric", 
+               values_to = "value") %>%
+  pivot_wider(names_from = model, values_from = value) %>%
+  mutate(metric = factor(metric, levels = c("coverage", "bias", "relbias"))) %>%
+  arrange (parameter, metric) %>%
+  mutate(metric = case_when(metric == "bias" ~ "absolute bias", 
+                            metric == "coverage" ~ "CI coverage (%)", 
+                            metric == "relbias" ~ "relative bias (%)"))
+
+# print metrics table
+metric_df_Simulx_4and2 %>% 
+  ungroup() %>%
+  dplyr::select(-parameter) %>%
+  kable(digits = 2, format = "html", table.attr = "style='width:60%;'") %>%
+  kable_styling(bootstrap_options = "striped") %>%
+  pack_rows("NPI 1", 1, 3) %>%
+  pack_rows("NPI 2", 4, 6) %>%
+  column_spec(1, width_min = "3.8cm")
