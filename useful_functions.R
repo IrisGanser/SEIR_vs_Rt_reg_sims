e125a6f9e5e45a62560882c1c3f4f4ecf4440d72
Iris Ganser <iris.ganser@mail.mcgill.ca>
2023-12-12 21:25:12 +0100

    added Rt_calc_fun

--- useful_functions.R
+++ useful_functions.R
@@ -207,7 +207,7 @@ EpiEstim_only_fun <- function(data_for_est, Inc_name, meansi = 7.5, stdsi = 5,
                              mean_si = meansi,
                              std_si = stdsi, 
                              mean_prior = meanprior, 
-                             str_prior = stdprior)))$R
+                             std_prior = stdprior)))$R
     Rt_estim <- Rt_estim %>%
       mutate(t = (t_end+t_start)/2,
              id = i, .before = t_start) %>%
@@ -219,5 +219,126 @@ EpiEstim_only_fun <- function(data_for_est, Inc_name, meansi = 7.5, stdsi = 5,
     rename(dept_id = id, day = t)
   
   return(Rt_df)
+}
+
+
+
+
+Rt_calc_fun <- function(Inc_df, id_col_name, time_col_name, Inc_col_name, model_name, 
+                        tstart = NULL, tend = NULL, Rt_ref_df, cut_days = 0,
+                        Rt_prior = 1, Rt_sd_prior = 2, meansi = 10.1, stdsi = 8.75){
+  # select input data according to desired input for Rt calculation
+  Inc_data <- Inc_df[c(id_col_name, time_col_name, Inc_col_name)]
+  colnames(Inc_data) <- c("id", "time", "Inc")
+  
+  # Rt calculation
+  Rt_list <- vector(mode = "list")
+  
+  if(is_null(tstart) | (is_null(tend))){
+    Rt_list <- foreach(i = 1:94, .packages = c("EpiEstim", "tidyverse")) %dopar% {
+      Rt_estim_I <- estimate_R(Inc_data$Inc[Inc_data$id == unique(Inc_data$id)[i]],
+                               method = "parametric_si",
+                               config = make_config(list(mean_si = meansi, 
+                                                         std_si = stdsi, 
+                                                         mean_prior = Rt_prior, 
+                                                         std_prior = Rt_sd_prior)))$R
+      Rt_estim_I <- Rt_estim_I %>%
+        mutate(t = (t_end+t_start)/2 + cut_days,
+               id = unique(Inc_data$id)[i], .before = t_start) %>%
+        select(t, t_start, t_end, id, `Mean(R)`, `Quantile.0.025(R)`, `Quantile.0.975(R)`) %>%
+        rename(Rt = `Mean(R)`, CI_LL = `Quantile.0.025(R)`, CI_UL = `Quantile.0.975(R)`)
+    } 
+  } 
+  
+  if(!is_null(tstart) & !(is_null(tend))){
+    Rt_list <- foreach(i = 1:94, .packages = c("EpiEstim", "tidyverse")) %dopar% {
+      Rt_estim_I <- estimate_R(Inc_data$Inc[Inc_data$id == unique(Inc_data$id)[i]],
+                               method = "parametric_si",
+                               config = make_config(list(
+                                 mean_si = meansi, std_si = stdsi, 
+                                 t_start = tstart, t_end = tend, 
+                                 mean_prior = Rt_prior, 
+                                 std_prior = Rt_sd_prior)))$R
+      Rt_estim_I <- Rt_estim_I %>%
+        mutate(t = (t_end+t_start)/2,
+               id = unique(Inc_data$id)[i], .before = t_start) %>%
+        select(t, t_start, t_end, id, `Mean(R)`, `Quantile.0.025(R)`, `Quantile.0.975(R)`) %>%
+        rename(Rt = `Mean(R)`, CI_LL = `Quantile.0.025(R)`, CI_UL = `Quantile.0.975(R)`)
+    } 
+  }
+  
+  Rt_df <- do.call("rbind.data.frame", Rt_list) %>%
+    rename(dept_id = id, day = t)
+  
+  Rt_comp_df <- Rt_df %>%
+    left_join(., Rt_ref_df, by = c("dept_id", "day")) %>%
+    mutate(Rt_residual = Rt - Rt_real, 
+           model = model_name)
+  
+  RMSE_df <- Rt_comp_df %>%
+    filter(!is.na(Rt)) %>%
+    group_by(dept_id, model) %>%
+    summarize(RMSE = sqrt(sum(Rt_residual^2)/n())) %>%
+    ungroup() %>%
+    mutate(RMSE_total = mean(RMSE))
+  
+  return(list(Rt_comp = Rt_comp_df, RMSE = RMSE_df))
+  
+}
+
+
+Rt_reg_only_fun <- function(data_for_est, lag_NPIs = FALSE, lag_days = 0, model_name, 
+                            fits = FALSE){
+  
+  if(lag_NPIs){
+    
+    reg_data <- data_for_est %>%
+      dplyr::select(dept_id, day, Rt, lockdown1, BG1) %>%
+      group_by(dept_id) %>%
+      mutate(lockdown1 = lag(lockdown1, n = lag_days, default = 0), 
+             BG1 = lag(BG1, n = lag_days, default = 0)) %>%
+      ungroup()
+    
+    reg_res <- lmer(log(Rt) ~ lockdown1 + BG1 + (1|dept_id), data = reg_data)
+    
+  } else{
+    
+    reg_data <- data_for_est %>%
+      dplyr::select(dept_id, day, Rt, lockdown1, BG1) 
+    
+    reg_res <- lmer(log(Rt) ~ lockdown1 + BG1 + (1|dept_id), data = reg_data)
+    
+  }
+  
+  
+  coefs_Rt_reg <- coefficients(reg_res)$dept_id
+  
+  confint_Rt_reg <- data.frame(confint(reg_res, method="Wald"))[-c(1:2), ]
+  names(confint_Rt_reg) <- c("CI_LL", "CI_UL")
+  
+  
+  coefs_Rt_reg <- coefs_Rt_reg %>%
+    dplyr::select(-1) %>%
+    unique() %>%
+    pivot_longer(cols = everything(), names_to = "parameter", values_to = "value") %>%
+    cbind(confint_Rt_reg[-1,]) %>%
+    mutate(parameter = factor(parameter,
+                              levels = c("lockdown1", "BG1"),
+                              labels = c("NPI 1", "NPI 2"))) %>%
+    mutate(lag = lag_days, model = model_name)
+  
+  
+  if(fits){
+    fitted_vals <- exp(fitted(reg_res))
+    fitted_df <- data_for_est %>%
+      filter(!is.na(Rt)) %>%
+      mutate(Rt_fitted = fitted_vals, lag = lag_days, model = model_name)
+    
+    return(list(coefs = coefs_Rt_reg, fits = fitted_df))
+  } else{
+    
+    return(coefs_Rt_reg)
+  }
+  
 }
 
